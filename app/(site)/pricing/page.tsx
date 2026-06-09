afd37b52c2d5f0512bbb68b4e038d59a1466a7f4
tahaawan0094 <tahaawan2026@gmail.com>
2026-06-09 18:04:20 +0500

    aaaaaaaaaaaaa

--- app/(site)/pricing/page.tsx
+++ app/(site)/pricing/page.tsx
@@ -2,12 +2,14 @@
 
 import PricingSection from "@/components/sections/PricingSection";
 import PageTransition from "@/components/animations/PageTransition";
+import FooterLinksSection from "@/components/sections/FooterLinksSection";
 
 export default function PricingPage() {
   return (
     <PageTransition>
       <main className="site">
         <PricingSection />
+        <FooterLinksSection />
       </main>
     </PageTransition>
   );
